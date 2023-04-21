9404a9515326a9eb106704a9dd9ebcc7425b6000
Fukuda Kouki <kouki.fukuda@sorcerer.co.jp>
2023-04-21 18:31:28 +0900

    migrationファイルの修正

--- migrations/sqls/20230421065032-create-player-item-table-up.sql
+++
@@ -1,7 +0,0 @@
-/* Replace with your SQL commands *//* Replace with your SQL commands */
-CREATE TABLE `player_items` (
-  `player_id` INT(11) UNSIGNED NOT NULL COMMENT "プレイヤーID",
-  `item_id`   INT(11) UNSIGNED NOT NULL COMMENT "アイテムID",
-  `count`     INT(11) UNSIGNED NOT NULL COMMENT "アイテム数",
-  PRIMARY KEY (`player_id`,`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
