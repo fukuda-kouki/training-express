770fe70b6f367d2613b983e654eb0742239a32c7
Fukuda Kouki <kouki.fukuda@sorcerer.co.jp>
2023-04-28 14:30:40 +0900

    useGachaのユニットテスト

--- tests/services/player-items-service.test.ts
+++ tests/services/player-items-service.test.ts
@@ -1,39 +1,55 @@
 import { NotEnoughError, UndefinedError } from "../../src/interfaces/my-error";
-import { PlayerItems } from "../../src/interfaces/player-items"
-import { addItem, useItem } from "../../src/services/player-items-service"
+import { addItem, useGacha, useItem } from "../../src/services/player-items-service"
 import * as playerItemsModel from "../../src/models/player-items-model"
 import * as playerModel from "../../src/models/player-model"
 import * as itemModel from "../../src/models/item-model"
+import * as randomHelper from "../../src/helpers/random-helper"
 import { Player } from "../../src/interfaces/player";
 import { Item } from "../../src/interfaces/item";
+import { PlayerItems } from "../../src/interfaces/player-items"
+import { Gacha } from "../../src/interfaces/gacha";
 
 const playerItemsUndefined: PlayerItems = {};
-const playerItemsData: PlayerItems = {
+const playerItemsData1: PlayerItems = {
   playerId: 1,
   itemId: 1,
   count: 1
 };
 
-const playerItemsUndefinedItemId: PlayerItems = { playerId: playerItemsData.playerId };
+const playerItemsData2: PlayerItems = {
+  playerId: 1,
+  itemId: 2,
+  count: 1
+};
+
+const playerItemsUndefinedItemId: PlayerItems = { playerId: playerItemsData1.playerId };
 const playerItemsUndefinedCount : PlayerItems = {
-  playerId: playerItemsData.playerId,
-  itemId: playerItemsData.itemId
+  playerId: playerItemsData1.playerId,
+  itemId: playerItemsData1.itemId
 };
 
 const playerData: Player = {
-  id: playerItemsData.playerId,
+  id: playerItemsData1.playerId,
   name: "user",
   hp: 10,
   mp: 10,
-  money: 10,
+  money: 200,
 };
 
-const itemData: Item = {
-  id: playerItemsData.itemId,
+const itemData1: Item = {
+  id: playerItemsData1.itemId,
   name: "hpPotion",
   heal: 100,
   price: 50,
   percent: 30
+}
+
+const itemData2: Item = {
+  id: playerItemsData1.itemId,
+  name: "mpPotion",
+  heal: 20,
+  price: 100,
+  percent: 70
 }
 
 jest
@@ -42,17 +58,17 @@ jest
 
 jest
 .spyOn(itemModel,"selectItemDataById")
-.mockResolvedValue(itemData);
+.mockResolvedValue(itemData1);
+
+const spyUpsertFunc = jest
+.spyOn(playerItemsModel, "insertOrIncrementData")
+.mockResolvedValue();
 
 describe('player-items-service:addItem', () => {
 
   jest
-  .spyOn(playerItemsModel, "insertOrIncrementData")
-  .mockResolvedValue();
-
-  jest
   .spyOn(playerItemsModel, "getCount")
-  .mockResolvedValue(playerItemsData.count!);
+  .mockResolvedValue(playerItemsData1.count!);
 
   test("playerId is Undefined", () => {
     let connection: any;
@@ -68,8 +84,8 @@ describe('player-items-service:addItem', () => {
 
   test("return value", () => {
     let connection: any;
-    expect(addItem(playerItemsData, connection))
-    .resolves.toBe(playerItemsData.count);
+    expect(addItem(playerItemsData1, connection))
+    .resolves.toBe(playerItemsData1.count);
   })
 })
 
@@ -89,23 +105,23 @@ describe('player-items-service:useItem', () => {
 
   const updatingData: Player = {
     id: useItemData.playerId,
-    hp: playerData.hp! + itemData.heal! * playerItemsData.count!,
+    hp: playerData.hp! + itemData1.heal! * playerItemsData1.count!,
     mp: playerData.mp
   };
 
   const retval = {
-    'itemId': itemData.id,
-    'count':  playerItemsData.count! - useItemData.count!,
+    'itemId': itemData1.id,
+    'count':  playerItemsData1.count! - useItemData.count!,
     'player': {
       'id': useItemData.playerId,
-      'hp': playerData.hp! + itemData.heal! * playerItemsData.count!,
+      'hp': playerData.hp! + itemData1.heal! * playerItemsData1.count!,
       'mp': playerData.mp
     }
   }
 
   jest
   .spyOn(playerItemsModel, "selectPlayerItemsDataById")
-  .mockResolvedValue(playerItemsData)
+  .mockResolvedValue(playerItemsData1)
   .mockResolvedValueOnce(playerItemsCountZero);
 
   jest
@@ -137,7 +153,7 @@ describe('player-items-service:useItem', () => {
     .rejects.toThrow(new NotEnoughError("useData.count larger than playeritemsData.count"));
   })
 
-  test ("updatePlayer args", async () => {
+  test ("updatePlayer args", () => {
     useItem(useItemData, connection)
     .then(() => {
       expect(playerModel.updatePlayer)
@@ -145,7 +161,7 @@ describe('player-items-service:useItem', () => {
     })
   })
 
-  test ("decrementData args", async () => {
+  test ("decrementData args", () => {
     useItem(useItemData, connection)
     .then(() => {
       expect(playerItemsModel.decrementData)
@@ -155,6 +171,129 @@ describe('player-items-service:useItem', () => {
 
   test ("return value", () => {
     expect(useItem(useItemData, connection))
+    .resolves.toEqual(retval);
+  })
+})
+
+describe("player-items-service:useGacha", () => {
+  const gachaRequestUndefined: Gacha = {};
+  const gachaRequestUndefinedCount: Gacha = { playerId: 1 };
+  const gachaRequest:Gacha = {
+    playerId: 1,
+    count: 2
+  }
+
+  const gahcaRequestCountOver:Gacha = {
+    playerId: 1,
+    count: 100
+  }
+
+  const updatingData:Player = {
+    id: gachaRequest.playerId,
+    money: playerData.money! - gachaRequest.count! * 10,
+  }
+
+  const upsertPlayerItemsData1: PlayerItems = {
+    playerId:1,
+    itemId:1,
+    count:1
+  }
+  const upsertPlayerItemsData2: PlayerItems = {
+    playerId: 1,
+    itemId: 2,
+    count: 1
+  }
+
+  const retval = {
+    'results': [
+      {
+        'itemId': 1,
+        'count': 1
+      },
+      {
+        'itemId': 2,
+        'count': 1
+      }
+    ],
+    'player' : {
+      'monay' : updatingData.money,
+      'items' : [
+        {
+          'itemId': 1,
+          'count': 1
+        },
+        {
+          'itemId': 2,
+          'count': 1
+        }
+      ]
+    }
+  }
+
+  jest
+  .spyOn(itemModel, "selectAllItems")
+  .mockResolvedValue([ itemData1, itemData2 ]);
+
+  jest
+  .spyOn(playerItemsModel, "selectPlayerItemsByPlayerId")
+  .mockResolvedValue([ playerItemsData1, playerItemsData2 ]);
+
+  const spyRandFunc = jest
+    .spyOn(randomHelper, "getRandomIntByRange")
+    .mockReturnValue(31);
+
+  let connection: any;
+  test("playerId is Undefined", () => {
+    expect(useGacha(gachaRequestUndefined, connection))
+    .rejects.toThrow(new UndefinedError("gachaRequest.playerId is undefined."));
+  })
+
+  test("count is Undefined", () => {
+    expect(useGacha(gachaRequestUndefinedCount, connection))
+    .rejects.toThrow(new UndefinedError("gachaRequest.count is undefined."));
+  })
+
+  test("playerData.money less than all gacha price.", () => {
+    expect(useGacha(gahcaRequestCountOver, connection))
+    .rejects.toThrow(new NotEnoughError("playerData.money less than all gacha price."));
+  })
+
+  test ("updatePlayer args", () => {
+    useGacha(gachaRequest, connection)
+    .then(() => {
+      expect(playerModel.updatePlayer)
+      .toBeCalledWith(updatingData, connection);
+    })
+  })
+
+  test ("getRandomIntByRange called times", () => {
+    spyRandFunc.mockClear();
+    useGacha(gachaRequest, connection)
+    .then(() => {
+      expect(randomHelper.getRandomIntByRange)
+      .toBeCalledTimes(gachaRequest.count!);
+    })
+  })
+
+  test ("1st time", () => {
+    spyUpsertFunc.mockClear();
+    spyRandFunc
+    .mockReturnValueOnce(30);
+
+    useGacha(gachaRequest, connection)
+    .then(() => {
+      expect(playerItemsModel.insertOrIncrementData)
+      .toHaveBeenNthCalledWith(1,upsertPlayerItemsData1, connection);
+      expect(playerItemsModel.insertOrIncrementData)
+      .toHaveBeenNthCalledWith(2,upsertPlayerItemsData2, connection);
+    })
+  })
+
+  test ("return value", () => {
+    spyRandFunc
+    .mockReturnValueOnce(30);
+
+    expect(useGacha(gachaRequest, connection))
     .resolves.toEqual(retval);
   })
 })
